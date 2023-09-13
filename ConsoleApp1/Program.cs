29c3bf534788b044769adf7cf84bd8317e4a5e6b
awhumayun <abd.humayun@gmail.com>
2023-09-13 15:34:51 -0400

    Add IsSum Function

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,4 +11,25 @@ string ReverseString(string s)
     return reverse;
 }
 
+bool IsSum(int[] a, int sum)
+{
+
+    for (int i = 0; i < a.Length; i++)
+    {
+        for (int j = 1; j < a.Length; j++)
+        {
+            if (a[i] + a[j] == sum && i != j)
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
 Console.WriteLine(ReverseString("Hello"));
+Console.WriteLine(IsSum(new int[] { 10, 8, 9, 17, 14, 6, 8, 3 }, 12));
+Console.WriteLine(IsSum(new int[] { 10, 8, 9, 17, 14, 6, 8, 3 }, 100));
+Console.WriteLine(IsSum(new int[] { 10, 8, 9, 17, 14, 6, 8, 3 }, 28));
+Console.WriteLine(IsSum(new int[] { 10, 8, 9, 17, 14, 6, 8, 3 }, 34));
+Console.WriteLine(IsSum(new int[] { 10, 8, 9, 17, 14, 6, 8, 3 }, 17 + 6));
