3d2588bfa78fe0aed901c0e42c30feab967ee7aa
awhumayun <abd.humayun@gmail.com>
2023-09-13 14:11:35 -0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+string ReverseString(string s)
+{
+    string reverse = "";
+
+    for (int i = s.Length - 1; i >= 0; i--)
+    {
+        reverse += s[i];
+    }
+
+    return reverse;
+}
+
+Console.WriteLine(ReverseString("Hello"));
